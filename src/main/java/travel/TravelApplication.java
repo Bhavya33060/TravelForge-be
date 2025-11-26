ee036f8dfe10c46cbbf57cda023b4b0f2f25cbd9
Bhavya33060 <yarlagaddabhavya27@gmail.com>
2025-11-26 14:43:21 +0530

    Update TravelApplication.java

--- src/main/java/travel/TravelApplication.java
+++ src/main/java/travel/TravelApplication.java
@@ -2,19 +2,13 @@ package travel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TravelApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(TravelApplication.class);
-    }
+public class TravelApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TravelApplication.class, args);
-        System.out.println("BACKEND SUCCESSFULLY STARTED");
+        System.out.println("🚀 BACKEND SUCCESSFULLY STARTED");
     }
 }
+
